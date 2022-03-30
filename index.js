b77279cd28c286d2d4ab41bd4c025b91428b673f
Sheena Bonilla <sheena.myb@gmail.com>
2022-03-30 16:39:43 -0700

    commit tree ebe59a1584d4a2076ef6e2dfabac3a767b65f265

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let num3 = 28;
+let num4 = 6;
+
+let multiply = num1 * num2;
+
+// random integer greater than 0
+let random = Math.floor(Math.random()) + 1;
+
+// calculate remainder of dividing variable
+let mod = Math.floor(num3 / num4);
+
+// find highest number in a set
+let max = Math.max(1, 2, 3, 5, 20);
